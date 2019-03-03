d28936ea2fee81914f92e90f3590d528acbdf919
RafailSkoulos17 <rafail.sk17@hotmail.gr>
2019-03-03 23:04:51 +0100

    Added script to extract inputs for the build of index. Added the directory with the results

---
+++ make_galago_input.py
@@ -0,0 +1,92 @@
+from trec_car import read_data
+
+
+def write_to_file(text, para_id, headline, f):
+    f.write(b'<DOC>\n')
+    f.write(b'<DOCNO> ' + bytes(str(para_id), 'utf8') + b' </DOCNO>\n')
+    f.write(b'<HEADLINE> ' + bytes(str(headline), 'utf8') + b' </HEADLINE>\n')
+    text = (text).encode('utf8')
+    f.write(b'<TEXT>\n')
+    f.write(text)
+    f.write(b'\n</TEXT>\n')
+    f.write(b'</DOC>\n')
+
+
+def hierarchical_iter_sections(section, headline, f):
+    headline += '/' + section.heading
+    for part in section.children:
+        if isinstance(part, read_data.Para):
+            para_text = part.get_text()
+            write_to_file(para_text, part.paragraph.para_id, headline, f)
+        elif isinstance(part, read_data.Section):
+            hierarchical_iter_sections(part, headline, f)
+
+
+def toplevel_iter_sections(section, headline, f):
+    for part in section.children:
+        if isinstance(part, read_data.Para):
+            para_text = part.get_text()
+            write_to_file(para_text, part.paragraph.para_id, headline, f)
+        elif isinstance(part, read_data.Section):
+            article_iter_sections(part, headline, f)
+
+
+def article_iter_sections(section, headline, f):
+    for part in section.children:
+        if isinstance(part, read_data.Para):
+            para_text = part.get_text()
+            write_to_file(para_text, part.paragraph.para_id, headline, f)
+        elif isinstance(part, read_data.Section):
+            article_iter_sections(part, headline, f)
+
+
+def build_article():
+    # read_file = r'benchmarkY1\benchmarkY1-train\train.pages.cbor'
+    read_file = r'test200-train\train.pages.cbor'
+    write_file = '2nd_attempt/article_index.trectext'
+    with open(write_file, 'wb') as f:
+        for page in read_data.iter_pages(open(read_file, 'rb')):
+            headline = page.page_name
+            for part in page.skeleton:
+                if isinstance(part, read_data.Para):
+                    page_text = part.get_text()
+                    write_to_file(page_text, part.paragraph.para_id, headline, f)
+                elif isinstance(part, read_data.Section):
+                    article_iter_sections(part, headline, f)
+
+
+def build_hierarchical():
+    # read_file = r'benchmarkY1\benchmarkY1-train\train.pages.cbor'
+    read_file = r'test200-train\train.pages.cbor'
+    write_file = '2nd_attempt/hierarchical_index.trectext'
+    with open(write_file, 'wb') as f:
+        for page in read_data.iter_pages(open(read_file, 'rb')):
+            for part in page.skeleton:
+                if isinstance(part, read_data.Para):
+                    page_text = part.get_text()
+                    headline = page.page_name
+                    write_to_file(page_text, part.paragraph.para_id, headline, f)
+                elif isinstance(part, read_data.Section):
+                    headline = page.page_name
+                    hierarchical_iter_sections(part, headline, f)
+
+
+def build_toplevel():
+    # read_file = r'benchmarkY1\benchmarkY1-train\train.pages.cbor'
+    read_file = r'test200-train\train.pages.cbor'
+    write_file = '2nd_attempt/toplevel_index.trectext'
+    with open(write_file, 'wb') as f:
+        for page in read_data.iter_pages(open(read_file, 'rb')):
+            for part in page.skeleton:
+                if isinstance(part, read_data.Para):
+                    page_text = part.get_text()
+                    headline = page.page_name
+                    write_to_file(page_text, part.paragraph.para_id, headline, f)
+                elif isinstance(part, read_data.Section):
+                    headline = str(page.page_name) + '/' + str(part.heading)
+                    toplevel_iter_sections(part, headline, f)
+
+
+build_article()
+build_hierarchical()
+build_toplevel()
